48e5aea9061ef77ff9eafab8eba9f379f552441b
rg <ryangehrin@gmail.com>
2015-04-06 15:12:31 -0700

    tt

--- main.rb
+++ main.rb
@@ -8,10 +8,10 @@ path = ENV["SDS_OUTPUT_PATH"]
 options_str = ENV["SDS_PLUGIN_CONFIG_JSON"]
 options = JSON.parse(options_str)
 sym_opts = options.inject({}){|memo,(k,v)| memo[k.to_sym] = v; memo}
-puts options
-puts "\n"
-puts sym_opts
-puts "\n"
+STDOUT.write options
+STDOUT.write  "\n"
+STDOUT.write sym_opts
+STDOUT.write  "\n"
 db_opts = sym_opts[:database]
 io_opts = sym_opts[:io]
 
