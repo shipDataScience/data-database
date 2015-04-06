0bf2b1f850a55870624f8f99030a621654f955c7
rg <ryangehrin@gmail.com>
2015-04-06 13:18:57 -0700

    tt

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,24 @@
-FROM ruby:2.1-onbuild
-CMD ["./main.rb"]
+# base on latest ruby base image
+FROM ruby:latest
+
+# update and install dependencies
+RUN apt-get update -qq
+RUN DEBIAN_FRONTEND=noninteractive apt-get install -y build-essential libpq-dev apt-utils
+
+apt-get install mysql-client postgresql-client libpq-dev libmysql-ruby libmysqlclient-dev
+sudo gem install pg mysql
+
+# setup app folders
+RUN mkdir /myapp
+WORKDIR /myapp
+
+# copy over Gemfile and install bundle
+ADD Gemfile /myapp/Gemfile
+ADD Gemfile.lock /myapp/Gemfile.lock
+RUN bundle install
+
+# copy over remaining app files
+ADD . /myapp
+
+# run script
+CMD [ "bundle exec", "main.rb" ]
--- main.rb
+++ main.rb
@@ -2,24 +2,38 @@ require 'sequel'
 require 'json'
 require 'csv'
 
+path = ENV["SDS_OUTPUT_PATH"]
 options_str = ENV["SDS_PLUGIN_CONFIG_JSON"]
 options = JSON.parse(options_str)
+sym_opts = options.inject({}){|memo,(k,v)| memo[k.to_sym] = v; memo}
+db_opts = sym_opts[:database]
+io_opts = sym_opts[:io]
 
-sql              = options[:sql]
-driver           = options[:adapter]
-user             = options[:user]
-password         = options[:password]
-host             = options[:host]
-port             = options[:port]
-database_name    = options[:databaseName]
-path =  = options[:path]
+# database
+sql_command      = db_opts[:sql]
+driver           = db_opts[:adapter]
+user             = db_opts[:user]
+password         = db_opts[:password]
+host             = db_opts[:host]
+port             = db_opts[:port]
+database_name    = db_opts[:databaseName]
 
-uri = "#{driver}://#{user}:#{password}@#{host}:#{port}/#{databaseName}"
+
+# io
+quote_char = io_opts[:quoteChar]
+delim_char = io_opts[:delimiter]
+headers = io_opts[:headers]
+quote_strategy = io_opts[:quoteStrategy]
+
+uri = "#{driver}://#{user}:#{password}@#{host}:#{port}/#{database_name}"
 
 DB = Sequel.connect(uri)
-data = DB.run(sql)
+data = DB.run(sql_command)
 
-CSV.open(path, 'wb') do |csv|
+CSV.open(path, 'wb', {
+    col_sep: delim_char,
+    quote_char: quote_char
+  }) do |csv|
   first = true
   data.each do |row|
     csv << row.inject([]) { |r, n| r << n[0] } and first = false if first
